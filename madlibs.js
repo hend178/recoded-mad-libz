635623e0df12eb10535a66e322419b09beff6db6
hind178 <hindmuhamed.178@gmail.com>
2022-08-26 10:49:19 +0300

    complete

--- madlibs.js
+++ madlibs.js
@@ -1,49 +1,102 @@
-/**
- * Complete the implementation of parseStory.
- *
- * parseStory retrieves the story as a single string from story.txt
- * (I have written this part for you).
- *
- * In your code, you are required (please read this carefully):
- * - to return a list of objects
- * - each object should definitely have a field, `word`
- * - each object should maybe have a field, `pos` (part of speech)
- *
- * So for example, the return value of this for the example story.txt
- * will be an object that looks like so (note the comma! periods should
- * be handled in the same way).
- *
- * Input: "Louis[n] went[v] to the store[n], and it was fun[a]."
- * Output: [
- *  { word: "Louis", pos: "noun" },
- *  { word: "went", pos: "verb", },
- *  { word: "to", },
- *  { word: "the", },
- *  { word: "store", pos: "noun" }
- *  { word: "," }
- *  ....
- *
- * There are multiple ways to do this, but you may want to use regular expressions.
- * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
- */
-function parseStory(rawStory) {
-  // Your code here.
-  return {}; // This line is currently wrong :)
+const OUR_REGEX = /(?<word>\w+)(?<pos>\[[nva]\])?(?<punctuation>[\.,])?/;
+console.log(OUR_REGEX)
+
+const POS = {
+  n: "noun",
+  v: "verb",
+  a: "adj",
+};
+function findPOS(pos) {
+  const fixedPOS = pos.replace("[", "").replace("]", "");
+  return POS[fixedPOS];
 }
 
-/**
- * All your other JavaScript code goes here, inside the function. Don't worry about
- * the `then` and `async` syntax for now.
- *
- * NOTE: You should not be writing any code in the global namespace EXCEPT
- * declaring functions. All code should either:
- * 1. Be in a function.
- * 2. Be in .then() below.
- *
- * You'll want to use the results of parseStory() to display the story on the page.
- */
+function parseStory(rawStory) {
+  const arrStory = rawStory.split(" ");
+
+  const fixedArray = [];
+  for (let el of arrStory) {
+    const group = OUR_REGEX.exec(el).groups;
+
+    if (group.pos != undefined && group.punc == undefined) {
+      fixedArray.push({
+        word: group.word,
+        pos: findPOS(group.pos),
+      });
+    } else if (group.pos == undefined && group.punc != undefined) {
+      fixedArray.push({
+        word: group.word,
+      });
+      fixedArray.push({
+        word: group.punc,
+      });
+    } else if (group.pos != undefined && group.punc != undefined) {
+      fixedArray.push({
+        word: group.word,
+        pos: findPOS(group.pos),
+      });
+      fixedArray.push({
+        word: group.punc,
+      });
+    } else {
+      fixedArray.push({
+        word: group.word,
+      });
+    }
+  }
+  console.log(fixedArray);
+  return fixedArray;
+}
+
 getRawStory()
   .then(parseStory)
   .then((processedStory) => {
+    const editDiv = document.querySelector(".madLibsEdit");
+    const previewDiv = document.querySelector(".madLibsPreview");
+    const storyEdit = document.createElement("p");
+    const storyPreview = document.createElement("p");
+    editDiv.append(storyEdit);
+    previewDiv.append(storyPreview);
+    storyEdit.className = "edit"
+    storyPreview.className = "edit"
+
+    for (let word of processedStory) {
+      if (word.pos != undefined) {
+        const inputEdit = document.createElement("input");
+        const inputPreview = document.createElement("input");
+
+        storyEdit.append(inputEdit);
+        storyPreview.append(inputPreview);
+        inputEdit.placeholder = word.pos;
+        inputPreview.placeholder = word.pos;
+        inputEdit.maxLength = 20;
+        inputPreview.readOnly = true;
+        inputEdit.className = "input";
+        inputPreview.className = "input";
+
+
+        inputEdit.addEventListener("input", () => {
+          inputPreview.value = inputEdit.value;
+        });
+
+        inputEdit.addEventListener("keydown", (event) => {
+          if (event.keyCode === 13) {
+            let input = inputEdit.nextElementSibling;
+
+            if (input != null) {
+              if (inputEdit.nodeName == "INPUT") {
+                input.focus();
+              }
+            } else {
+              alert("Game Done!");
+            }
+          }
+        });
+      } else {
+        storyEdit.append(`${word.word} `);
+        storyPreview.append(`${word.word} `);
+      }
+    }
+
     console.log(processedStory);
   });
